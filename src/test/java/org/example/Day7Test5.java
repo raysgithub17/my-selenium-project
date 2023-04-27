19060983889345dbc8ee161b5ff92ac6e605d578
raysgithub17 <smdrayhan2002@gmail.com>
2023-04-27 12:31:27 +0530

    day3

--- src/test/java/org/example/Day7Test5.java
+++ src/test/java/org/example/Day7Test5.java
@@ -10,10 +10,19 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class Day7Test4 {
+public class Day7Test5 {
     WebDriver driver;
+    //In Test1 we have to verify that the page is loaded successfully
     @Test
-    public void test1() throws InterruptedException {
+    public void testcase1(){
+        String Actual_link = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+        String Expected_link = driver.getCurrentUrl();
+        Assert.assertEquals(Actual_link,Expected_link);
+        System.out.println("testcase1 successfull!!!");
+    }
+    //In Test2 we are going to check whether the page is logout successfully
+    @Test
+    public void testcase2() throws InterruptedException {
 
         //Enter user name
         WebElement user_id = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input"));
@@ -27,11 +36,23 @@ public class Day7Test4 {
         WebElement login = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button"));
         login.click();
 
-        //Assert whether the profile link and current link is same
-        String profile_link = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index";
-        String current_link = driver.getCurrentUrl();
-        Assert.assertEquals(current_link,profile_link);
-        System.out.println("Successfull!!");
+        Thread.sleep(3000);
+
+        //click dropdown
+        WebElement drop = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/header/div[1]/div[2]/ul/li/span/i"));
+        drop.click();
+
+        //click logout
+        WebElement logout = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/header/div[1]/div[2]/ul/li/ul/li[4]/a"));
+        logout.click();
+
+        //verify whether the page is logout
+        Thread.sleep(3000);
+        String Actual_link = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+        String Expected_link = driver.getCurrentUrl();
+        Assert.assertEquals(Actual_link,Expected_link);
+        System.out.println("testcase 2 log-out!!!");
+
 
     }
     //Before test open url and maximize the screen
@@ -49,5 +70,4 @@ public class Day7Test4 {
     public void afterTest() {
         driver.close();
     }
-
 }
